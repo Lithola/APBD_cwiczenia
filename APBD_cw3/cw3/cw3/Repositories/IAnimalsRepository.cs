e972dfe5d7f78d9b21d291f5a667d1cfa6d085c9
Lithola <97567933+Lithola@users.noreply.github.com>
2024-04-27 23:46:04 +0200

    added repository

---
+++ APBD_cw3/cw3/cw3/Repositories/IAnimalsRepository.cs
@@ -0,0 +1,12 @@
+using cw3.Models;
+
+namespace cw3.Repositories;
+
+public interface IAnimalsRepository
+{
+    IEnumerable<Animal> GetAnimals();
+    int CreateAnimal(Animal animal);
+    Animal GetAnimal(int idAnimal);
+    int UpdateAnimal(Animal animal);
+    int DeleteAnimal(int idAnimal);
+}
